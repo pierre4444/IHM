d1aae94128ed7c842de659f58afae276b0cca587
AlexisJTSE <alexis6630@gmail.com>
2020-02-11 11:59:04 +0100

    Page d'accueil

--- Ardoise/app/src/main/java/com/example/ardoise/MainActivity.java
+++ Ardoise/app/src/main/java/com/example/ardoise/MainActivity.java
@@ -3,12 +3,53 @@ package com.example.ardoise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mHelloText;
+    private EditText mConnexionText;
+    private Button mConnexionButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHelloText = (TextView) findViewById(R.id.hello_text);
+        mConnexionText = (EditText) findViewById(R.id.connexion_text);
+        mConnexionButton = (Button) findViewById(R.id.connexion_button);
+
+        mConnexionButton.setEnabled(false);
+
+        mConnexionText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mConnexionButton.setEnabled(s.toString().length() != 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mConnexionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
